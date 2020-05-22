96ac6d68aae2d7b672ebf9bef83d2468e5e56299
raf <rafoid@gmail.com>
2020-05-22 22:51:53 +0000

    dev container changes for PG

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 sudo chown vscode -R $(pwd)
 
-./configure --enable-debug; make; sudo make install
+./configure --enable-debug
+make
+sudo make install
+sudo mkdir $PGDATA
+sudo chown -R postgres:postgres $PGDATA
+/usr/local/pgsql/bin/initdb
+cd
+/usr/local/pgsql/bin/pg_ctl -l logfile start
+/usr/local/pgsql/bin/createdb test
+
+sudo chown -R postgres:postgres $WS
 
 echo "Appending github.ibm.com to ~/.ssh/known_hosts"
 mkdir ~/.ssh && /usr/bin/ssh-keyscan -H github.com >> ~/.ssh/known_hosts
