058061cadfd0a915bfce5a5b2894ec7480af0fe3
Hugh Nguyen <hughnguyen@ibm.com>
2020-06-02 23:37:22 +0000

    Change github.ibm.com to github.com

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -12,5 +12,5 @@ cd
 
 sudo chown -R postgres:postgres $WS
 
-echo "Appending github.ibm.com to ~/.ssh/known_hosts"
+echo "Appending github.com to ~/.ssh/known_hosts"
 mkdir ~/.ssh && /usr/bin/ssh-keyscan -H github.com >> ~/.ssh/known_hosts
